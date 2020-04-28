524233f1849d77c9398571c8be7a9709b9e08569
Andrii Maliuta <quadr988@gmail.com>
2020-04-28 15:49:03 +0300

    init

--- src/main/java/com/anma/js/blogs/sbjsblogs/controllers/BlogController.java
+++ src/main/java/com/anma/js/blogs/sbjsblogs/controllers/BlogController.java
@@ -1,6 +1,8 @@
 package com.anma.js.blogs.sbjsblogs.controllers;
 
+import com.anma.js.blogs.sbjsblogs.BlogRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/blogs")
 public class BlogController {
 
+    private final BlogRepository blogRepository;
+
+    public BlogController(BlogRepository blogRepository) {
+        this.blogRepository = blogRepository;
+    }
+
     @GetMapping
-    public String blogs() {
+    public String blogs(Model model) {
+        model.addAttribute("blogs", blogRepository.findAll());
         return "blogs";
     }
 
---
+++ src/main/java/com/anma/js/blogs/sbjsblogs/rest/api/BlogsRestController.java
@@ -0,0 +1,27 @@
+package com.anma.js.blogs.sbjsblogs.rest.api;
+
+import com.anma.js.blogs.sbjsblogs.BlogRepository;
+import com.anma.js.blogs.sbjsblogs.models.Blog;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(BlogsRestController.REST_URL)
+public class BlogsRestController {
+
+    final static String REST_URL = "/rest/api/va/blogs";
+
+    private final BlogRepository blogRepository;
+
+    public BlogsRestController(BlogRepository blogRepository) {
+        this.blogRepository = blogRepository;
+    }
+
+    @GetMapping
+    public List<Blog> getAllBlogs() {
+        return blogRepository.findAll();
+    }
+}
